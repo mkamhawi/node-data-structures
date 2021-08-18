832d805e3ddf34235919f4bfa9cfcd0885ad5b36
Mohamed El-Kamhawi <mkamhawie@gmail.com>
2021-08-18 03:31:15 +0200

    Binary search tree traversal

--- src/datastructures/bst.js
+++ src/datastructures/bst.js
@@ -42,4 +42,28 @@ module.exports = class BinarySearchTree {
   getRightChild() {
     return this.rightChild;
   }
+
+  traverseInOrder() {
+    let result = '';
+    result += this.leftChild ? this.leftChild.traverseInOrder() : '';
+    result += `${this.value} `;
+    result += this.rightChild ? this.rightChild.traverseInOrder() : '';
+    return result;
+  }
+
+  traversePreOrder() {
+    let result = '';
+    result += `${this.value} `;
+    result += this.leftChild ? this.leftChild.traversePreOrder() : '';
+    result += this.rightChild ? this.rightChild.traversePreOrder() : '';
+    return result;
+  }
+
+  traversePostOrder() {
+    let result = '';
+    result += this.leftChild ? this.leftChild.traversePostOrder() : '';
+    result += this.rightChild ? this.rightChild.traversePostOrder() : '';
+    result += `${this.value} `;
+    return result;
+  }
 };
--- tests/unit/bst.spec.js
+++ tests/unit/bst.spec.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const BinarySearchTree = require('../../src/datastructures/bst');
 
 describe('Binary Search Trees', () => {
-  context('Node insertion order', () => {
+  context('Insert', () => {
     it('Should insert nodes in correct order', () => {
       const root = new BinarySearchTree(33);
       root.insert(34);
@@ -15,6 +15,42 @@ describe('Binary Search Trees', () => {
       expect(root.getRightChild().getValue()).to.be.eql(34);
       expect(root.getLeftChild().getLeftChild().getValue()).to.be.eql(11);
       expect(root.getLeftChild().getRightChild().getValue()).to.be.eql(25);
+    });
+  });
+
+  context('In order traversal', () => {
+    it('Should return expected traversal string', () => {
+      const root = new BinarySearchTree(33);
+      root.insert(34);
+      root.insert(22);
+      root.insert(11);
+      root.insert(25);
+
+      expect(root.traverseInOrder()).to.be.eql('11 22 25 33 34 ');
+    });
+  });
+
+  context('Pre order traversal', () => {
+    it('Should return expected traversal string', () => {
+      const root = new BinarySearchTree(33);
+      root.insert(34);
+      root.insert(22);
+      root.insert(11);
+      root.insert(25);
+
+      expect(root.traversePreOrder()).to.be.eql('33 22 11 25 34 ');
+    });
+  });
+
+  context('Post order traversal', () => {
+    it('Should return expected traversal string', () => {
+      const root = new BinarySearchTree(33);
+      root.insert(34);
+      root.insert(22);
+      root.insert(11);
+      root.insert(25);
+
+      expect(root.traversePostOrder()).to.be.eql('11 25 22 34 33 ');
     });
   });
 });
