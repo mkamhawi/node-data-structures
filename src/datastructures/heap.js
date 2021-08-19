bedf490c2a94390072e3f72ad449de0beb2f6b12
Mohamed El-Kamhawi <mkamhawie@gmail.com>
2021-08-19 02:38:24 +0200

    Heap

---
+++ src/datastructures/heap.js
@@ -0,0 +1,63 @@
+module.exports = class Heap {
+  constructor() {
+    this.heap = [];
+  }
+
+  pushToHeap(item) {
+    const i = this.heap.length;
+    this.heap[i] = item;
+    this.balanceChildPlacement(i);
+  }
+
+  pullHeapRoot() {
+    const root = this.heap.shift();
+    const lastLeaf = this.heap.pop();
+    this.heap.splice(0, 0, lastLeaf);
+    this.balanceParentPlacement(0);
+    return root;
+  }
+
+  async balanceParentPlacement(parentIndex) {
+    const leftChildIndex = 2 * parentIndex + 1;
+    const rightChildIndex = 2 * parentIndex + 2;
+    if (this.heap.length <= leftChildIndex) return;
+    const rightChildExist = this.heap.length > rightChildIndex;
+    if (
+      this.heap[parentIndex] < this.heap[leftChildIndex]
+      || (rightChildExist && this.heap[parentIndex] < this.heap[rightChildIndex])
+    ) {
+      let swapTargetIndex;
+      if (!rightChildExist) swapTargetIndex = leftChildIndex;
+      else {
+        swapTargetIndex = this.heap[leftChildIndex] > this.heap[rightChildIndex]
+          ? leftChildIndex
+          : rightChildIndex;
+      }
+      this.swapItems(parentIndex, swapTargetIndex);
+      await this.balanceParentPlacement(swapTargetIndex);
+    }
+  }
+
+  async balanceChildPlacement(childIndex) {
+    const isLeftChild = childIndex % 2 === 1;
+    const parentIndex = (childIndex - (isLeftChild ? 1 : 2)) / 2;
+    if (this.heap[parentIndex] < this.heap[childIndex]) {
+      this.swapItems(childIndex, parentIndex);
+      await this.balanceChildPlacement(parentIndex);
+    }
+  }
+
+  swapItems(i, j) {
+    const tmp = this.heap[i];
+    this.heap[i] = this.heap[j];
+    this.heap[j] = tmp;
+  }
+
+  getHeapSize() {
+    return this.heap.length;
+  }
+
+  getHeap() {
+    return this.heap;
+  }
+};
---
+++ tests/unit/heap.spec.js
@@ -0,0 +1,57 @@
+const { expect } = require('chai');
+const Heap = require('../../src/datastructures/heap');
+
+describe('Heap tests', () => {
+  context('Pushing to heap', () => {
+    it('Should push items to heap in expected order', () => {
+      const heap = new Heap();
+      heap.pushToHeap(33);
+      expect(heap.getHeap()).to.eql([33]);
+      heap.pushToHeap(30);
+      expect(heap.getHeap()).to.eql([33, 30]);
+      heap.pushToHeap(30);
+      expect(heap.getHeap()).to.eql([33, 30, 30]);
+      heap.pushToHeap(11);
+      expect(heap.getHeap()).to.eql([33, 30, 30, 11]);
+    });
+
+    it('Should rebalance heap correctly after pushing an item', () => {
+      const heap = new Heap();
+      heap.pushToHeap(33);
+      heap.pushToHeap(30);
+      heap.pushToHeap(11);
+      expect(heap.getHeap()).to.eql([33, 30, 11]);
+      heap.pushToHeap(31);
+      expect(heap.getHeap()).to.eql([33, 31, 11, 30]);
+      heap.pushToHeap(34);
+      expect(heap.getHeap()).to.eql([34, 33, 11, 30, 31]);
+      heap.pushToHeap(22);
+      expect(heap.getHeap()).to.eql([34, 33, 22, 30, 31, 11]);
+    });
+  });
+
+  context('Pulling heap root', () => {
+    it('Should rebalance heap correctly after pulling root', () => {
+      const heap = new Heap();
+      heap.pushToHeap(33);
+      heap.pushToHeap(30);
+      heap.pushToHeap(11);
+      heap.pushToHeap(31);
+      heap.pushToHeap(34);
+      heap.pushToHeap(22);
+      expect(heap.getHeap()).to.eql([34, 33, 22, 30, 31, 11]);
+      let root = heap.pullHeapRoot();
+      expect(root).to.be.eql(34);
+      expect(heap.getHeap()).to.eql([33, 31, 22, 30, 11]);
+      root = heap.pullHeapRoot();
+      expect(root).to.be.eql(33);
+      expect(heap.getHeap()).to.eql([31, 30, 22, 11]);
+      root = heap.pullHeapRoot();
+      expect(root).to.be.eql(31);
+      expect(heap.getHeap()).to.eql([30, 11, 22]);
+      root = heap.pullHeapRoot();
+      expect(root).to.be.eql(30);
+      expect(heap.getHeap()).to.eql([22, 11]);
+    });
+  });
+});
